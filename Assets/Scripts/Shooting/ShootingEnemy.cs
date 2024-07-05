391efb1ad58cf9dd6c3a71f2b9d4cb0b5ab8a8ee
sin49 <ngo533gute@gmail.com>
2024-07-05 14:46:19 +0900

    123123
    1231233

--- Assets/Scripts/Shooting/ShootingEnemy.cs
+++ Assets/Scripts/Shooting/ShootingEnemy.cs
@@ -9,14 +9,40 @@ public class ShootingEnemy : ShootingObject
     public float movelooptime;
     float movelooptimer;
     int direction = 1;
+    bool onviewport;
+
+    public float snappoint=0.6f;
     public override void Start()
     {
         base.Start();
         AttackTimer = attacktime;
     }
-
+    void DeactiveOutsideViewport()
+    {
+        if (this.transform.localPosition.x < ShootingFIeld.instance.MaxSizeX+snappoint || this.transform.localPosition.x > ShootingFIeld.instance.MinSizeX- snappoint ||
+            this.transform.localPosition.y < ShootingFIeld.instance.MaxSizeY + snappoint || this.transform.localPosition.y > ShootingFIeld.instance.MinSizeY- snappoint)
+            onviewport = false;
+        else
+            onviewport = true;
+       
+    }
     private void FixedUpdate()
     {
+
+        movelooptimer += Time.deltaTime;
+        if (movelooptimer >= movelooptime)
+        {
+            movelooptimer = 0;
+            direction *= -1;
+        }
+        transform.Translate(Vector3.right * direction * movespeed * Time.deltaTime);
+        DeactiveOutsideViewport();
+
+       
+
+        if (!onviewport)
+            return;
+
         this.transform.position = new Vector3(transform.position.x, transform.position.y, ShootingPlayer.instance.transform.position.z);
         TargetVector = (ShootingPlayer.instance.transform.position - transform.position).normalized;
         if(!onshoot)
@@ -26,11 +52,6 @@ public class ShootingEnemy : ShootingObject
             StartCoroutine(Attack());
             AttackTimer = attacktime;
         }
-        movelooptimer += Time.deltaTime;
-        if( movelooptimer >=movelooptime) {
-            movelooptimer = 0;
-            direction *= -1;
-        }
-        transform.Translate(Vector3.right*direction * movespeed * Time.deltaTime);
+      
     }
 }
--- Assets/ShootingFIeld.cs
+++ Assets/ShootingFIeld.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class ShootingFIeld : MonoBehaviour
 {
+    public static ShootingFIeld instance;
+
     public Vector2 FieldSize;
     public Vector2 Center;
 
@@ -13,16 +15,28 @@ public class ShootingFIeld : MonoBehaviour
    public float MinSizeX;
     public float MaxSizeY;
     public float MinSizeY;
+
+    public float SnapPoint;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     private void Update()
     {
         GetSize();
     }
     void GetSize()
     {
-        MaxSizeX = this.transform.position.x + Center.x + FieldSize.x;
-        MinSizeX= this.transform.position.x + Center.x - FieldSize.x;
-        MaxSizeY= this.transform.position.y + Center.y + FieldSize.y;
-        MinSizeY= this.transform.position.y + Center.y - FieldSize.y;
+        //MaxSizeX = this.transform.position.x + Center.x + FieldSize.x;
+        //MinSizeX= this.transform.position.x + Center.x - FieldSize.x;
+        //MaxSizeY= this.transform.position.y + Center.y + FieldSize.y;
+        //MinSizeY= this.transform.position.y + Center.y - FieldSize.y;
+        MaxSizeX =  (Center.x + FieldSize.x)/2- SnapPoint;
+        MinSizeX = -1 * (Center.x + FieldSize.x) / 2+ SnapPoint;
+        MaxSizeY = ( FieldSize.y)/2+Center.y- SnapPoint;
+        MinSizeY =  -1* ( FieldSize.y) / 2+Center.y+ SnapPoint;
     }
     private void OnDrawGizmos()
     {
