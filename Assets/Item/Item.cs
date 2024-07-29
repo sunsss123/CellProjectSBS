c1ce3cbc650048868dea728b7d2bffd9d316f04c
sin49 <ngo533gute@gmail.com>
2024-07-30 08:31:09 +0900

    1421414
    412414

--- Assets/Item/Item.cs
+++ Assets/Item/Item.cs
@@ -10,42 +10,4 @@ public class item : ScriptableObject
     public string itemname;
     public string itemdescription;
 }
-[Serializable]
-[CreateAssetMenu(fileName = "New Essetial Intem", menuName = "Custom/new Essetial Item")]
-public class Essentialitem : item
-{
-    public string itemcode;
-}
 
-[Serializable]
-[CreateAssetMenu(fileName = "New MUltiPly item", menuName = "Custom/new MUltiPly item")]
-public class MUltiPlyitem: item
-{
-
-
-    public float InitItemPower;
-    public float ItemPower;
-    public UpgradeStatus upgradeStatus;
-    void ItemEffect(int itemnumber)
-    {
-        switch (upgradeStatus)
-        {
-            case UpgradeStatus.Energy:
-                    PlayerStat.instance.HPBonus -= ReturnItemPower(itemnumber - 1); 
-                PlayerStat.instance.HPBonus += ReturnItemPower(itemnumber);
-                break;
-                case UpgradeStatus.MoveSpeed:
-                PlayerStat.instance.MoveSpeedBonus -= ReturnItemPower(itemnumber - 1);
-                PlayerStat.instance.MoveSpeedBonus += ReturnItemPower(itemnumber);
-                break;
-        }
-    }
-    public void GetItem(int number)
-    {
-
-        ItemEffect(number);
-    }
-    float ReturnItemPower(int number) {  if (number <= 0) return 0;
-        Debug.Log("숫자"+number+"계산 값" +( InitItemPower + ItemPower * number)); return InitItemPower + ItemPower * number;  }
-
-}
