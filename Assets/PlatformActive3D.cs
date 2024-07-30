908293393f00f24b344e9d732b2dd471d063bc27
LDJ729 <ldj4689@naver.com>
2024-07-30 12:39:32 +0900

    07.30.2
    1239

--- Assets/PlatformActive3D.cs
+++ Assets/PlatformActive3D.cs
@@ -2,28 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformActive3D : PlatformMoveByDimesion
+public class PlatformActive3D : MonoBehaviour
 {
     BoxCollider Bcollider;
-   
-
+    public float Zmove = 1;
+    public bool BcolliderActive3D=true;
     private void Awake()
     {
         Bcollider = GetComponent<BoxCollider>();
     }
-    public override void PlatformChange3D()
+    void PlatformChange3D()
     {
-        base.PlatformChange3D();
+        if (BcolliderActive3D) 
         Bcollider.enabled = true;
+        else
+            Bcollider.enabled = false;
+        transform.Translate(Vector3.back * Zmove);
     }
-    public override void PlatformChange2D()
+    void PlatformChange2D()
     {
-        base.PlatformChange2D();
-        Bcollider.enabled = false;
+        if (BcolliderActive3D)
+            Bcollider.enabled = false;
+        else
+            Bcollider.enabled = true;
+        transform.Translate(Vector3.forward * Zmove);
     }
-    
-       
- 
-       
-    
+
+   
+    void Update()
+    {
+        if (BcolliderActive3D)
+        {
+            if (!Bcollider.enabled && PlayerStat.instance.Trans3D)
+            {
+                PlatformChange3D();
+            }
+            else if (Bcollider.enabled && !PlayerStat.instance.Trans3D)
+            {
+                PlatformChange2D();
+            }
+        }
+        else
+        {
+            if (Bcollider.enabled && PlayerStat.instance.Trans3D)
+            {
+                PlatformChange3D();
+            }
+            else if (!Bcollider.enabled && !PlayerStat.instance.Trans3D)
+            {
+                PlatformChange2D();
+            }
+        }
+    }
 }
